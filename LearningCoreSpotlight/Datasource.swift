8e5219d3884fe37dc95e84d47f3c0b0cfea1fa2e
Rommel Rico <me@rommelrico.com>
2017-10-09 10:40:58 -0700

    Assets for the sample app

---
+++ LearningCoreSpotlight/Datasource.swift
@@ -0,0 +1,13 @@
+//
+//  Datasource.swift
+//  LearningCoreSpotlight
+//
+//  Created by Rommel Rico on 10/9/17.
+//  Copyright © 2017 Rommel Rico. All rights reserved.
+//
+
+import UIKit
+
+class Datasource: NSObject {
+
+}
